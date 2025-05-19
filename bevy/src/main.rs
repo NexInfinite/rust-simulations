8fe047a14be9ba8bfacbbebff33f1fade7bdf6d5
Julian Jones <julianjones663@gmail.com>
2025-05-19 23:37:44 +0100

    what a mess, bed time

--- bevy/src/main.rs
+++ bevy/src/main.rs
@@ -1,4 +1,10 @@
-use bevy::{color::palettes::css::GREEN, math::ops::powf, prelude::*, render::camera::Viewport};
+use bevy::{
+    color::palettes::css::{DARK_GRAY, GREEN},
+    math::ops::powf,
+    prelude::*,
+    render::camera::Viewport,
+    transform,
+};
 
 fn main() {
     App::new()
@@ -21,11 +27,14 @@ fn controls(
     input: Res<ButtonInput<KeyCode>>,
     time: Res<Time<Fixed>>,
     mut exit: EventWriter<AppExit>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let Ok(_) = window.single() else {
+    let Ok(window) = window.single() else {
         return;
     };
-    let Ok((_, mut transform, mut projection)) = camera_query.single_mut() else {
+    let Ok((mut camera, mut transform, mut projection)) = camera_query.single_mut() else {
         return;
     };
     let f32_speed = 400.0 * time.delta_secs();
@@ -57,6 +66,59 @@ fn controls(
 
         if input.pressed(KeyCode::Period) {
             projection2d.scale *= powf(0.25, time.delta_secs());
+        }
+
+        // XY White Lines
+        commands.spawn((
+            Mesh2d(meshes.add(Rectangle::new(
+                1.0,
+                (window.height() + f32::abs(transform.translation.y) * 2.0) * projection2d.scale,
+            ))),
+            MeshMaterial2d(materials.add(Color::srgb(0.75, 0.75, 0.75))),
+            Transform::from_translation(Vec3::new(0.0, 0.0, -1000.0)),
+        ));
+        commands.spawn((
+            Mesh2d(meshes.add(Rectangle::new(
+                (window.width() + f32::abs(transform.translation.x) * 2.0) * projection2d.scale,
+                1.0,
+            ))),
+            MeshMaterial2d(materials.add(Color::srgb(0.75, 0.75, 0.75))),
+            Transform::from_translation(Vec3::new(0.0, 0.0, -1000.0)),
+        ));
+
+        // Gray lines every 500 pixels
+        let step = 100;
+        for i in (step..(window.width() / 2.0 * projection2d.scale) as i32).step_by(step as usize) {
+            commands.spawn((
+                Mesh2d(meshes.add(Rectangle::new(
+                    1.0,
+                    (window.height() + f32::abs(transform.translation.y) * 2.0)
+                        * projection2d.scale,
+                ))),
+                MeshMaterial2d(materials.add(Color::srgb(0.25, 0.25, 0.25))),
+                Transform::from_translation(Vec3::new(i as f32, 0.0, -1000.0)),
+            ));
+            commands.spawn((
+                Mesh2d(meshes.add(Rectangle::new(
+                    1.0,
+                    (window.height() + f32::abs(transform.translation.y) * 2.0)
+                        * projection2d.scale,
+                ))),
+                MeshMaterial2d(materials.add(Color::srgb(0.25, 0.25, 0.25))),
+                Transform::from_translation(Vec3::new(-i as f32, 0.0, -1000.0)),
+            ));
+        }
+
+        for i in (step..(window.height() / 2.0 * projection2d.scale) as i32).step_by(step as usize)
+        {
+            commands.spawn((
+                Mesh2d(meshes.add(Rectangle::new(
+                    (window.width() + f32::abs(transform.translation.x) * 2.0) * projection2d.scale,
+                    1.0,
+                ))),
+                MeshMaterial2d(materials.add(Color::srgb(0.25, 0.25, 0.25))),
+                Transform::from_translation(Vec3::new(i as f32, 0.0, -1000.0)),
+            ));
         }
     }
 }
