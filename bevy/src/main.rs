b361a6faa03b798c5fdb1a941ed6d1ad32139be7
Julian Jones <julianjones663@gmail.com>
2025-05-21 19:52:39 +0100

    added zooming

--- bevy/src/main.rs
+++ bevy/src/main.rs
@@ -1,4 +1,5 @@
 use bevy::{
+    math::ops::powf,
     prelude::*,
     reflect::TypePath,
     render::render_resource::{AsBindGroup, ShaderRef},
@@ -10,12 +11,10 @@ const SHADER_ASSET_PATH: &str = "shaders/custom_material.wgsl";
 
 fn main() {
     App::new()
-        .add_plugins((
-            DefaultPlugins,
-            Material2dPlugin::<CustomMaterial>::default(),
-        ))
+        .add_plugins((DefaultPlugins, Material2dPlugin::<GridShader>::default()))
         .add_systems(Startup, setup)
         .add_systems(Update, scale_background)
+        .add_systems(Update, controls)
         .run();
 }
 
@@ -23,7 +22,7 @@ fn main() {
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<CustomMaterial>>,
+    mut materials: ResMut<Assets<GridShader>>,
     window: Single<&Window>,
 ) {
     // Camera and window
@@ -33,16 +32,44 @@ fn setup(
     let window_size = window.resolution.physical_size().as_vec2();
     commands.spawn((
         Mesh2d(meshes.add(Rectangle::default())),
-        MeshMaterial2d(materials.add(CustomMaterial {
+        MeshMaterial2d(materials.add(GridShader {
             window_size: window_size,
+            zoom: 1.0,
         })),
         Transform::from_scale(vec3(window_size.x, window_size.y, 0.0)),
     ));
 }
 
+fn controls(
+    mut materials: ResMut<Assets<GridShader>>,
+    input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time<Fixed>>,
+    mut exit: EventWriter<AppExit>,
+) {
+    // Exiting
+    if input.pressed(KeyCode::Escape) {
+        exit.write(AppExit::Success);
+    }
+
+    // Camera zoom controls
+    if input.pressed(KeyCode::Comma) {
+        for material in materials.iter_mut() {
+            material.1.zoom *= powf(4.0, time.delta_secs());
+            material.1.zoom = f32::clamp(material.1.zoom, 0.2, 5.0);
+        }
+    }
+
+    if input.pressed(KeyCode::Period) {
+        for material in materials.iter_mut() {
+            material.1.zoom *= powf(0.25, time.delta_secs());
+            material.1.zoom = f32::clamp(material.1.zoom, 0.2, 5.0);
+        }
+    }
+}
+
 fn scale_background(
     mut query: Query<&mut Transform, With<Mesh2d>>,
-    mut materials: ResMut<Assets<CustomMaterial>>,
+    mut materials: ResMut<Assets<GridShader>>,
     window: Single<&Window>,
 ) {
     let window_size = window.resolution.physical_size().as_vec2();
@@ -56,14 +83,16 @@ fn scale_background(
 
 // This is the struct that will be passed to your shader
 #[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
-struct CustomMaterial {
+struct GridShader {
     #[uniform(0)]
     window_size: Vec2,
+    #[uniform(1)]
+    zoom: f32,
 }
 
 /// The Material2d trait is very configurable, but comes with sensible defaults for all methods.
 /// You only need to implement functions for features that need non-default behaviour. See the Material2d api docs for details!
-impl Material2d for CustomMaterial {
+impl Material2d for GridShader {
     fn fragment_shader() -> ShaderRef {
         SHADER_ASSET_PATH.into()
     }
