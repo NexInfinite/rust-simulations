307eba263ef3adb782115ff8d7022fb6071d6b57
Julian Jones <julianjones663@gmail.com>
2025-05-24 22:31:43 +0100

    made button

--- bevy/src/main.rs
+++ bevy/src/main.rs
@@ -1,12 +1,15 @@
 use bevy::prelude::*;
+
 mod camera;
 mod circular;
 mod grid;
+mod ui;
 
 fn main() {
     App::new()
         .add_plugins((
             DefaultPlugins,
+            ui::UiPlugin,
             camera::CameraPlugin,
             grid::GridPlugin,
             circular::CircularPlugin,
---
+++ bevy/src/ui.rs
@@ -0,0 +1,65 @@
+use bevy::{
+    color::palettes::css::{BLACK, WHITE},
+    prelude::*,
+};
+
+pub struct UiPlugin;
+
+impl Plugin for UiPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup);
+        app.add_systems(Update, button_system);
+    }
+}
+
+fn setup(mut commands: Commands, assets: Res<AssetServer>) {
+    commands.spawn(button(&assets));
+}
+
+fn button(_asset_server: &AssetServer) -> impl Bundle + use<> {
+    (
+        Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            ..default()
+        },
+        children![(
+            Button,
+            Node {
+                position_type: PositionType::Absolute,
+                top: Val::Px(15.0),
+                left: Val::Px(15.0),
+                width: Val::Px(100.0),
+                height: Val::Px(45.0),
+                border: UiRect::all(Val::Px(2.0)),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            BorderColor(Color::from(WHITE)),
+            BackgroundColor(Color::from(BLACK)),
+            children![(
+                Text::new("Pause"),
+                TextFont {
+                    font_size: 24.0,
+                    ..default()
+                },
+                TextColor(Color::from(WHITE)),
+            )]
+        )],
+    )
+}
+
+fn button_system(
+    mut interaction_query: Query<(&Interaction, &Children), (Changed<Interaction>, With<Button>)>,
+    mut text_query: Query<&mut Text>,
+) {
+    for (interaction, children) in &mut interaction_query {
+        let mut text = text_query.get_mut(children[0]).unwrap();
+        if matches!(interaction, Interaction::Pressed) {
+            **text = "Press".to_string();
+        }
+    }
+}
